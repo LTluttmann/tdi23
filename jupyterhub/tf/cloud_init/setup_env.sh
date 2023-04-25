b36b5541b62364c8de44bead93e28d51aff09fd3
Laurin Luttmann <laurin.luttmann@gmail.com>
2023-04-25 12:49:18 +0200

    get hook on vm during startup

--- jupyterhub/tf/cloud_init/setup_env.sh
+++ jupyterhub/tf/cloud_init/setup_env.sh
@@ -2,7 +2,9 @@
 
 # need to install GitPython in the right environment, 
 # thus explicitly name it here
-/opt/tljh/hub/bin/python -m pip install GitPython
+sudo /opt/tljh/hub/bin/python -m pip install GitPython
 
-git clone 
+# curl the python hook file for cloning repo in user spaces
+sudo curl -L https://raw.githubusercontent.com/LTluttmann/tdi23/main/jupyterhub/hooks/clone_repo_hook.py > hook.py
+sudo mv hook.py /opt/tljh/config/jupyterhub_config.d/
 
