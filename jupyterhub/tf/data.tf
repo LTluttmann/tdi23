5f83232d0cbdff2bd87e0dfd9d5ca55b47a09f78
Laurin Luttmann <laurin.luttmann@gmail.com>
2023-04-25 18:40:28 +0200

    cleanup

--- jupyterhub/tf/data.tf
+++ jupyterhub/tf/data.tf
@@ -2,18 +2,6 @@ data "cloudinit_config" "config" {
   gzip          = true
   base64_encode = true
 
-  # part {
-  #   filename     = "env.cfg"
-  #   content_type = "text/x-shellscript"
-  #   content      = "${file("${path.module}/cloud_init/setup_jupyterhub.sh")}"
-  # }
-
-  # part {
-  #   filename     = "user.cfg"
-  #   content_type = "text/x-shellscript"
-  #   content      = "${file("${path.module}/cloud_init/add_users.sh")}"
-  # }
-
   part {
     filename     = "env.cfg"
     content_type = "text/x-shellscript"
